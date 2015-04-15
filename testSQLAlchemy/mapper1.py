ba45806d657ad2b37ccef58099ceb7e4bed0845e
Corentin Berger <corent.berger@gmail.com>
2015-04-15 15:12:49 +0200

    test

--- testSQLAlchemy/mapper1.py
+++ testSQLAlchemy/mapper1.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import mapper, sessionmaker
 
 db = create_engine( 'mysql://Dirac:corent@127.0.0.1/testDiracDB' )
 
-db.echo = True
+# db.echo = True
 
 metadata = MetaData( db )
 
@@ -29,6 +29,8 @@ emailmapper = mapper( Email, emails )
 Session = sessionmaker( bind = db )
 session = Session()
 
+s = users.select( users.c.name == 'John' )
+run( s )
 
 print ''
 print 'fred'
@@ -37,11 +39,17 @@ fred = User()
 fred.name = 'Fred'
 fred.age = 37
 
-print "About to commit() without a nadd()..."
+print "About to commit() without an add()..."
 session.commit()  # Will *not* save Fred's data yet
+s = users.select( users.c.name == 'Fred' )
+run( s )
+
 
 session.add( fred )
 session.commit()  # Now Fred's data will be saved
+
+s = users.select( users.c.name == 'Fred' )
+run( s )
 
 session.delete( fred )
 session.flush()
